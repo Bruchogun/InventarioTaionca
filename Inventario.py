2a848e58efc793b02624f79b81da2eed15c6abdb
unknown <mauroalejandro20@gmail.com>
2020-04-10 15:15:01 -0500

    New Button: Editar Inventario (100% finished) New Button: Consumir Inventario (Finished Interface but no actions)

--- Inventario.py
+++ Inventario.py
@@ -17,12 +17,12 @@ height =int(((height-150)/2)-(height/10))
 
 #--------------------------------------CodeCannotBeRepeated---------------------------------------#
 def codevalidation(code):
-    dataBase=pd.read_csv('Base de datos.csv',header=0)
+    dataBase=pd.read_csv('Base de datos.exe',header=0)
     lista=dataBase['Código']
     if code==lista:
         answer=-1
     return(answer)
-#----------------------------------Añadir Inventario-----------------------------------#
+#----------------------------------Añadir Artículo-----------------------------------#
 def write_Data_csvFile(add_item_code,add_item_description,add_item_SellPriceODT,add_item_SellPricePub,add_item_PriceCost,add_item_Stock,actual_selection,inventarioW,labelError):
     code=str(add_item_code.get())
     description=add_item_description.get()
@@ -35,13 +35,13 @@ def write_Data_csvFile(add_item_code,add_item_description,add_item_SellPriceODT,
 
     #----------------------------------Information Verifier/DataBase Saver-----------------------------------#
     
-    dataBase=pd.read_csv('Base de datos.csv',header=0)
+    dataBase=pd.read_csv('Base de datos.exe',header=0)
     send_counter=0
     lista=dataBase['Codigo']
     if (code==lista).any():
         send_counter=3
     while sellPriceODT.isdigit() and sellPricePub.isdigit() and priceCost.isdigit() and stock.isdigit() and send_counter<1 and measure !='Ninguna'and sellPriceODT != "" and sellPricePub != "" and priceCost != "" and stock != "" and code != "":
-        dataBase= open('Base de datos.csv','a',encoding="utf-8")
+        dataBase= open('Base de datos.exe','a',encoding="utf-8")
         dataBase.write(code)
         dataBase.write(',')
         dataBase.write(description)
@@ -72,8 +72,51 @@ def write_Data_csvFile(add_item_code,add_item_description,add_item_SellPriceODT,
     elif send_counter == 3:
         labelError.grid(row=8,column=1)
     else:
-        labelError.destroy()
+        labelError.grid_forget()
 
+def edit_data(add_item_code,add_item_description,add_item_SellPriceODT,add_item_SellPricePub,add_item_PriceCost,add_item_Stock,actual_selection,inventarioW,labelError):
+    code=str(add_item_code.get())
+    description=add_item_description.get()
+    sellPriceODT=add_item_SellPriceODT.get()
+    sellPricePub=add_item_SellPricePub.get()
+    priceCost=add_item_PriceCost.get()
+    stock=add_item_Stock.get()
+    measure=actual_selection.get()
+    today=str(date.today())
+
+    #----------------------------------Information Verifier/DataBase Saver-----------------------------------#
+    dataBase=pd.read_csv('Base de datos.exe',header=0)
+    send_counter=0
+    lista=dataBase['Codigo']
+    if code in lista.to_list():
+        while sellPriceODT.isdigit() and sellPricePub.isdigit() and priceCost.isdigit() and stock.isdigit() and send_counter<1 and measure !='Ninguna'and sellPriceODT != "" and sellPricePub != "" and priceCost != "" and stock != "" and code != "":
+            place=0
+            for row in range(len(lista)):       
+                if code == lista[row]: 
+                    place=row
+            dataBase['Descripcion'][place]=description
+            dataBase['Precio de Venta a ODTs'][place]=sellPriceODT
+            dataBase['Precio de Venta al Publico'][place]=sellPricePub
+            dataBase['Precio de Costo'][place]=priceCost
+            dataBase['Existencias'][place]=stock
+            dataBase['Unidad de Medida'][place]=measure
+            dataBase['Ultima Fecha de Modificacion'][place]=today
+            dataBase.to_csv('Base de datos.exe', index=0)
+
+            add_item_code.delete(0,1000)
+            add_item_description.delete(0,1000)
+            add_item_SellPriceODT.delete(0,1000)
+            add_item_SellPricePub.delete(0,1000)
+            add_item_PriceCost.delete(0,1000)
+            add_item_Stock.delete(0,1000)
+            actual_selection.set('Ninguna')
+            send_counter=1
+    else:
+        send_counter=3
+    if send_counter == 0 or send_counter==3:
+        labelError.grid(row=8,column=1)
+    else:
+        labelError.grid_forget()
     
 def send_button(password,add_item_button,delete_item_button,edit_item_button,view_item_button,inventarioW,passwordW):
     dataBase=pd.read_excel('Registro de Contraseñas.xlsx','A')
@@ -138,7 +181,7 @@ def add_item(add_item_button,delete_item_button,edit_item_button,view_item_butto
 
     #-------------------------Buttons------------------------------------------#
     labelError=tkinter.Label(inventarioW,text='Los valores ingresados no son válidos.')
-    send_button4=tkinter.Button(inventarioW,text='↑ Añadir Inventario ↑',command=lambda :write_Data_csvFile(add_item_code,add_item_description,add_item_SellPriceODT,add_item_SellPricePub,add_item_PriceCost,add_item_Stock,actual_selection,inventarioW,labelError))
+    send_button4=tkinter.Button(inventarioW,text='↑ Enviar ↑',command=lambda :write_Data_csvFile(add_item_code,add_item_description,add_item_SellPriceODT,add_item_SellPricePub,add_item_PriceCost,add_item_Stock,actual_selection,inventarioW,labelError))
     send_button4.grid(row=7,column=1)
 
 #------------------------------------Limpiar Pantalla--------------------------------------#
@@ -172,7 +215,7 @@ def sotckView(inventarioW,add_item_button,delete_item_button,edit_item_button,vi
     table.heading('#7', text='Última Modificación', anchor = 'center' )
     table.column('#7',anchor='center')
 
-    dataBase=pd.read_csv('Base de datos.csv',header=0)
+    dataBase=pd.read_csv('Base de datos.exe',header=0)
     lista=[]
     parameter=len(dataBase['Codigo'])
     for row in range(parameter):
@@ -183,7 +226,68 @@ def sotckView(inventarioW,add_item_button,delete_item_button,edit_item_button,vi
         table.insert('','end', text=code, values=(lista))
         lista.clear()
     
+#----------------------------------------Editar Artículo-------------------------------------#
+def edit_item(add_item_button,delete_item_button,edit_item_button,view_item_button,inventarioW):
+        if 1==1:
+            cleanOut(add_item_button,delete_item_button,edit_item_button,view_item_button)
+            #---------------------LABEL------------------------------------------------#
+            add_item_labelCode=tkinter.Label(inventarioW,text='Código')
+            add_item_labelCode.grid(row=0,column=0)
+            add_item_labelDescription=tkinter.Label(inventarioW,text='Descripción del Artículo')
+            add_item_labelDescription.grid(row=1,column=0)
+            add_item_labelSellPriceODT=tkinter.Label(inventarioW,text="Precio de Venta a ODT's en $")
+            add_item_labelSellPriceODT.grid(row=2,column=0)
+            add_item_labelSellPricePub=tkinter.Label(inventarioW,text='Precio de venta al Público en $')
+            add_item_labelSellPricePub.grid(row=3,column=0)
+            add_item_labelPriceCost=tkinter.Label(inventarioW,text='Precio de Costo en $')
+            add_item_labelPriceCost.grid(row=4,column=0)
+            add_item_labelStock=tkinter.Label(inventarioW,text='Existencias Disponibles')
+            add_item_labelStock.grid(row=5,column=0)
+            #-------------------------BoxEntry-----------------------------------------#
+            add_item_code=tkinter.Entry(inventarioW)
+            add_item_code.grid(row=0,column=1)
+            add_item_description=tkinter.Entry(inventarioW)
+            add_item_description.grid(row=1,column=1)
+            add_item_SellPriceODT=tkinter.Entry(inventarioW)
+            add_item_SellPriceODT.grid(row=2,column=1)
+            add_item_SellPricePub=tkinter.Entry(inventarioW)
+            add_item_SellPricePub.grid(row=3,column=1)
+            add_item_PriceCost=tkinter.Entry(inventarioW)
+            add_item_PriceCost.grid(row=4,column=1)
+            add_item_Stock=tkinter.Entry(inventarioW)
+            add_item_Stock.grid(row=5,column=1)    
 
+            #---------------------------DeployBar--------------------------------------#
+            actual_selection=tkinter.StringVar(inventarioW)
+            actual_selection.set('Ninguna')
+            selection=tkinter.StringVar(inventarioW)
+            measure_selection=['Unidad (Und)','Kilogramo (Kg)','Metro Cúbico (m^3)','Metro (m)']
+            selection=tkinter.OptionMenu(inventarioW,actual_selection,*measure_selection)
+            selection.grid(row=6,column=1)
+
+            #-------------------------Buttons------------------------------------------#
+            labelError=tkinter.Label(inventarioW,text='Los valores ingresados no son válidos.')
+            send_button4=tkinter.Button(inventarioW,text='↑ Enviar ↑',command=lambda :edit_data(add_item_code,add_item_description,add_item_SellPriceODT,add_item_SellPricePub,add_item_PriceCost,add_item_Stock,actual_selection,inventarioW,labelError))
+            send_button4.grid(row=7,column=1)
+        else:
+            messagebox.showwarning('Error', 'Usted no tiene acceso a esta opción')
+
+#------------------------------------Consumir Inventario-------------------------------------#
+def stock_consume(inventarioW,add_item_button,delete_item_button,edit_item_button,view_item_button):
+    cleanOut(add_item_button,delete_item_button,edit_item_button,view_item_button)
+    
+    consume_code=tkinter.Label(inventarioW,text='Código')
+    consume_code.grid(row=0,column=0)
+    consume_quantity=tkinter.Label(inventarioW,text='Cantidad consumida')
+    consume_quantity.grid(row=1,column=0)
+    consume_ODT=tkinter.Label(inventarioW,text="ODT de consumo")
+    consume_ODT.grid(row=2,column=0)
+    consume_code_Entry=tkinter.Entry(inventarioW)
+    consume_code_Entry.grid(row=0,column=1)
+    consume_quantity_Entry=tkinter.Entry(inventarioW)
+    consume_quantity_Entry.grid(row=1,column=1)
+    consume_ODT_Entry=tkinter.Entry(inventarioW)
+    consume_ODT_Entry.grid(row=2,column=1)
 
 #------------------------------------Función Inventario--------------------------------------#
 def inventario():
@@ -194,10 +298,10 @@ def inventario():
     add_item_button=tkinter.Button(inventarioW, text='Añadir Artículo', font='Helvetica 10', command = lambda: verification(add_item_button,delete_item_button,edit_item_button,view_item_button,inventarioW))
     add_item_button.place(relx=0.25,rely=0.25,relwidth=0.25, relheight=0.25)
 
-    delete_item_button=tkinter.Button(inventarioW, text='Soon', font='Helvetica 10')
+    delete_item_button=tkinter.Button(inventarioW, text='Consumir Inventario', font='Helvetica 10',command = lambda:stock_consume(inventarioW,add_item_button,delete_item_button,edit_item_button,view_item_button))
     delete_item_button.place(relx=0.5,rely=0.5,relwidth=0.25, relheight=0.25)
 
-    edit_item_button=tkinter.Button(inventarioW, text='Soon', font='Helvetica 10')
+    edit_item_button=tkinter.Button(inventarioW, text='Editar Artículo', font='Helvetica 10',command = lambda: edit_item(add_item_button,delete_item_button,edit_item_button,view_item_button,inventarioW))
     edit_item_button.place(relx=0.25,rely=0.5,relwidth=0.25, relheight=0.25)
 
     view_item_button=tkinter.Button(inventarioW, text='Ver Stock', font='Helvetica 10', command = lambda: sotckView(inventarioW,add_item_button,delete_item_button,edit_item_button,view_item_button))
